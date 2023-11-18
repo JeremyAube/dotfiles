54569a9f3f3d4709f3e5a15b242404f90d99bc47
Jeremy Aube <jeremyaube@pm.me>
2023-11-17 23:19:35 -0500

    Add keybind to git push and pull

--- nvim/lua/jeremyaube/plugins/fugitive.lua
+++ nvim/lua/jeremyaube/plugins/fugitive.lua
@@ -1,11 +1,14 @@
 return {
-  "tpope/vim-fugitive",
-  depedendencies = { "tpope/vim-rhubarb" },
-  config = function()
-    vim.keymap.set("n", "<leader>go", ":Git<cr>")
-    vim.keymap.set("n", "<leader>gc", ":Git commit<cr>")
-    vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
-    vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<cr>")
-    vim.keymap.set("n", "<leader>gv", ":GBrowse<cr>")
-  end
+	"tpope/vim-fugitive",
+	dependencies = { "tpope/vim-rhubarb" },
+	config = function()
+		vim.keymap.set("n", "<leader>go", ":Git<cr>")
+		vim.keymap.set("n", "<leader>gc", ":Git commit<cr>")
+		vim.keymap.set("n", "<leader>gP", ":Git push<cr>")
+		vim.keymap.set("n", "<leader>gp", ":Git pull<cr>")
+
+		vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
+		vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<cr>")
+		vim.keymap.set("n", "<leader>gi", ":GBrowse<cr>")
+	end,
 }
