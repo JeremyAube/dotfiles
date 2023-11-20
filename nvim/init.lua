4139209424f9641c14b23cd4380bfe4e99be8a93
Jeremy Aube <jeremyaube@pm.me>
2023-11-19 21:51:18 -0500

    Added some fonctionnality based on kickstart.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,11 +2,23 @@ require("jeremyaube.keybinds")
 require("jeremyaube.lazy")
 
 vim.opt.termguicolors = true
-vim.cmd("colorscheme kanagawa")
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.colorcolumn = "100"
 vim.wo.wrap = false
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = "yes"
+vim.o.completeopt = "menuone,noselect"
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
 
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
--- nvim/lua/jeremyaube/keybinds.lua
+++ nvim/lua/jeremyaube/keybinds.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "<leader>cd", ":lua vim.diagnostic.open_float()<cr>")
 vim.keymap.set("n", "<enter>", ":noh<cr>")
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>cl", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
