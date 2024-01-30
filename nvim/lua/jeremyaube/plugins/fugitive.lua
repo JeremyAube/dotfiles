22cc05933b33cb6eb4ba3c9d2844698c3c23b7db
Jeremy Aube <jeremyaube@pm.me>
2024-01-29 21:22:54 -0500

    Modify git keymaps

--- nvim/lua/jeremyaube/plugins/fugitive.lua
+++ nvim/lua/jeremyaube/plugins/fugitive.lua
@@ -4,8 +4,7 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>go", ":Git<cr>")
 		vim.keymap.set("n", "<leader>gc", ":Git commit<cr>")
-		vim.keymap.set("n", "<leader>gP", ":Git push<cr>")
-		vim.keymap.set("n", "<leader>gp", ":Git pull<cr>")
+		vim.keymap.set("n", "<leader>ga", ":Git ")
 
 		vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
 		vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<cr>")
