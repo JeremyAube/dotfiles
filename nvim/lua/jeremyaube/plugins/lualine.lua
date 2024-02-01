ed6bf83202f2ae3f4357c9615173d8506522c80d
Jeremy Aube <jeremyaube@pm.me>
2024-01-31 23:47:50 -0500

    Update lualine appearance

--- nvim/lua/jeremyaube/plugins/lualine.lua
+++ nvim/lua/jeremyaube/plugins/lualine.lua
@@ -1,20 +1,19 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-  config = function()
-    require("lualine").setup({
-      options = {
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "filetype" },
-        lualine_y = { "encoding" },
-        lualine_z = { "location" },
-      },
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	opts = {
+		theme = "auto",
+		options = {
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "|", right = "" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff" },
+			lualine_c = { "filename", "diagnostics" },
+			lualine_x = { "filetype" },
+			lualine_y = { "encoding" },
+			lualine_z = { "datetime" },
+		},
+	},
 }
