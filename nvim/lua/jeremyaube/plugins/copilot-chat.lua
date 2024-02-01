94c5d343d186912265051167e8a6ccd5d8216dca
Jeremy Aube <jeremyaube@pm.me>
2024-01-31 23:48:05 -0500

    Switch to jellydn fork of CopilotChat

--- nvim/lua/jeremyaube/plugins/copilot-chat.lua
+++ nvim/lua/jeremyaube/plugins/copilot-chat.lua
@@ -1,6 +1,22 @@
 return {
-	"gptlang/CopilotChat.nvim",
-	config = function()
-		vim.keymap.set("n", "<leader>i", ":CopilotChat ")
+	"jellydn/CopilotChat.nvim",
+	opts = {
+		mode = "split",
+		prompts = {
+			Explain = "Explain how it works.",
+			Tests = "Briefly explain how the selected code works, then generate unit tests.",
+		},
+	},
+	build = function()
+		vim.defer_fn(function()
+			vim.cmd("UpdateRemotePlugins")
+			vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
+		end, 3000)
 	end,
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>ic", ":CopilotChat " },
+		{ "<leader>ie", "<cmd>CopilotChatExplain<cr>" },
+		{ "<leader>it", "<cmd>CopilotChatTest<cr>" },
+	},
 }
