4171aaf91f6864e1276531da0572dc9aacc86a1b
Jeremy Aube <jeremyaube@pm.me>
2024-03-08 21:42:59 -0500

    Update lualine setup to add rounded corners

--- nvim/lua/jeremyaube/plugins/lualine.lua
+++ nvim/lua/jeremyaube/plugins/lualine.lua
@@ -4,16 +4,16 @@ return {
 	opts = {
 		theme = "auto",
 		options = {
-			section_separators = { left = "", right = "" },
-			component_separators = { left = "|", right = "" },
+			section_separators = { left = "", right = "" },
+			component_separators = "",
 		},
 		sections = {
-			lualine_a = { "mode" },
+			lualine_a = { { "mode", separator = { left = "", right = "" }, right_padding = 2 } },
 			lualine_b = { "branch", "diff" },
 			lualine_c = { "filename", "diagnostics" },
-			lualine_x = { "filetype" },
-			lualine_y = { "encoding" },
-			lualine_z = { "datetime" },
+			lualine_x = {},
+			lualine_y = { "filetype" },
+			lualine_z = { { "encoding", separator = { left = "", right = "" }, left_padding = 2 } },
 		},
 	},
 }
