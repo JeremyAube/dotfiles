f9f6b339a3a134a0f07034407f068b9666a69721
Jeremy Aube <jeremyaube@pm.me>
2024-03-12 14:36:42 -0400

    Update copilot chat to new version

--- nvim/lua/jeremyaube/plugins/copilot-chat.lua
+++ nvim/lua/jeremyaube/plugins/copilot-chat.lua
@@ -1,10 +1,61 @@
 return {
-	"jellydn/CopilotChat.nvim",
+	"CopilotC-Nvim/CopilotChat.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	},
 	opts = {
 		mode = "split",
 		prompts = {
-			Explain = "Explain how it works.",
-			Tests = "Briefly explain how the selected code works, then generate unit tests.",
+			Explain = {
+				prompt = "/COPILOT_EXPLAIN Write a explanation for the code above as paragraphs of text.",
+				mapping = "<leader>ie",
+			},
+			Tests = {
+				prompt = "/COPILOT_TESTS Write a set of detailed unit test functions for the code above.",
+				mapping = "<leader>it",
+				selection = function(source)
+					return require("CopilotChat.select").buffer(source)
+				end,
+			},
+			Fix = {
+				prompt = "/COPILOT_FIX There is a problem in this code. Rewrite the code to show it with the bug fixed.",
+				mapping = "<leader>if",
+			},
+			FixDiagnostic = {
+				prompt = "Please assist with the following diagnostic issue in file:",
+				mapping = "<leader>id",
+				selection = function(source)
+					return require("CopilotChat.select").diagnostics(source)
+				end,
+			},
+			CommitStaged = {
+				prompt = "Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
+				mapping = "<leader>id",
+				selection = function(source)
+					return require("CopilotChat.select").gitdiff(source, true)
+				end,
+			},
+		},
+		window = {
+			layout = "vertical", -- 'vertical', 'horizontal', 'float'
+			-- relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
+			-- border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+			-- width = 0.8, -- fractional width of parent
+			-- height = 0.6, -- fractional height of parent
+			-- row = nil, -- row position of the window, default is centered
+			-- col = nil, -- column position of the window, default is centered
+			title = "Copilot Chat", -- title of chat window
+			-- footer = nil, -- footer of chat window
+			zindex = 1, -- determines if window is on top or below other floating windows
+		},
+		-- default mappings
+		mappings = {
+			close = "q",
+			reset = "<C-l>",
+			complete = "<Tab>",
+			submit_prompt = "<CR>",
+			accept_diff = "<C-y>",
+			show_diff = "<C-t>",
 		},
 	},
 	build = function()
@@ -16,7 +67,6 @@ return {
 	event = "VeryLazy",
 	keys = {
 		{ "<leader>ic", ":CopilotChat " },
-		{ "<leader>ie", "<cmd>CopilotChatExplain<cr>" },
-		{ "<leader>it", "<cmd>CopilotChatTest<cr>" },
+		{ "<leader>io", ":CopilotChatToggle<CR>" },
 	},
 }
