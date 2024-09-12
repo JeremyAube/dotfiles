2cca2824eba5bc2dca34182ad4ab06993777fab4
Jeremy Aube <jeremyaube@pm.me>
2024-09-11 22:46:24 -0400

    feat [wezterm]: update to use custom Iosevka font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,17 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night Moon"
-config.font = wezterm.font("Iosevka Nerd Font")
-config.font_size = 19
+config.font = wezterm.font({
+	family = "IosevkaCustom Nerd Font",
+	weight = "Medium",
+})
+
+config.font_size = 18
+
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 
--- and finally, return the configuration to wezterm
 return config
