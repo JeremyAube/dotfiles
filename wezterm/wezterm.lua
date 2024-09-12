fec09de78e585e6d941cb17e7e799d410e23285c
Jeremy Aube <jeremyaube@pm.me>
2024-09-11 22:49:05 -0400

    fix [wezterm]: use Tokyonight colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,12 +2,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.color_scheme = "Tokyo Night"
 
 config.font = wezterm.font({
 	family = "IosevkaCustom Nerd Font",
 	weight = "Medium",
 })
-
 config.font_size = 18
 
 config.enable_tab_bar = false
