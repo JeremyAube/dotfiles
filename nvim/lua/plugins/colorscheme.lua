65a10fb097b39b3318aaf7beac4e1c8af9bbd2e1
Jeremy Aube <jeremyaube@pm.me>
2025-04-07 22:16:15 -0400

    [colorscheme]: change back to Rasmus, it's too good

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,37 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"kvrohit/rasmus.nvim",
 	priority = 1000,
 	config = function()
-		require("rose-pine").setup({
-			variant = "moon",
-			dim_inactive_window = true,
-
-			styles = {
-				bold = true,
-				italic = false,
-				transparency = false,
-			},
-
-			highlight_groups = {
-				Comment = { italic = true },
-			},
-		})
-
-		vim.cmd.colorscheme("rose-pine")
+		vim.g.rasmus_bold_keywords = true
+		vim.g.rasmus_italic_functions = true
+		vim.g.rasmus_transparent = true
+		vim.cmd([[colorscheme rasmus]])
 	end,
 }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			variant = "moon",
+-- 			dim_inactive_window = true,
+--
+-- 			styles = {
+-- 				bold = true,
+-- 				italic = false,
+-- 				transparency = false,
+-- 			},
+--
+-- 			highlight_groups = {
+-- 				Comment = { italic = true },
+-- 			},
+-- 		})
+--
+-- 		vim.cmd.colorscheme("rose-pine")
+-- 	end,
+-- }
 
 -- return {
 -- 	"jesseleite/nvim-noirbuddy",
