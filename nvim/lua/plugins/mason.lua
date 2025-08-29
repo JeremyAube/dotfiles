1f7d84934b2ea4fb3afac72dee01aff640eeff51
Jeremy Aube <jeremyaube@pm.me>
2025-08-28 20:27:13 -0400

    [neovim]: Setup Expert nightly LSP server for elixir

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,10 +8,20 @@ return {
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
 			ensure_installed = {
-				"elixirls",
+				"lexical",
 				"ts_ls",
 				"lua_ls",
 			},
+		})
+
+		require("lspconfig").lexical.setup({
+			cmd = { "/Users/jeremy/.local/bin/expert_darwin_arm64" },
+			root_dir = function(fname)
+				return require("lspconfig").util.root_pattern("mix.exs", ".git")(fname) or vim.loop.cwd()
+			end,
+			filetypes = { "elixir", "eelixir", "heex" },
+			-- optional settings
+			settings = {},
 		})
 	end,
 }
