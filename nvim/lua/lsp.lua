48481656c3ce61422d82d5dce70ef750d58bb6e1
Jeremy Aube <jeremyaube@pm.me>
2025-10-28 23:56:04 -0400

    [neovim]: move telescope lsp keymaps to telescope config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,18 +1,6 @@
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-		if client:supports_method("textDocument/implementation") then
-			vim.keymap.set("n", "grr", "<cmd>Telescope lsp_references theme=ivy<cr>", { buffer = args.buf })
-			vim.keymap.set("n", "grc", "<cmd>Telescope lsp_incoming_calls theme=ivy<cr>", { buffer = args.buf })
-		end
-	end,
-})
-
 vim.diagnostic.config({
 	title = false,
 	underline = true,
-	virtual_text = true,
 	signs = {
 		text = {
 			[vim.diagnostic.severity.ERROR] = " ",
@@ -32,6 +20,3 @@ vim.diagnostic.config({
 		prefix = "",
 	},
 })
-
--- Custom language servers
-vim.lsp.enable("expert")
