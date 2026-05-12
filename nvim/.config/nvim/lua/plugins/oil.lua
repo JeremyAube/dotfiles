48cb617efc86bbe7a885b0a8ca4703f93ff5d853
Jeremy Aube <jeremyaube@pm.me>
2026-05-11 20:57:24 -0400

    nvim: config cleanup and update plugins

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,27 +1,20 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "oil",
-			callback = function()
-				vim.lsp.stop_client(vim.lsp.get_active_clients({ bufnr = 0 }))
-			end,
-		})
-
-		require("oil").setup({
-			default_file_explorer = true,
-			column = { "icon" },
-			delete_to_trash = true,
-			constrain_cursor = "name",
-			view_options = {
-				show_hidden = true,
-			},
-			float = {
-				padding = 5,
-				border = "rounded",
-			},
-		})
-		vim.keymap.set("n", "-", vim.cmd.Oil, { desc = "Open file explorer" })
-	end,
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      column = { "icon" },
+      delete_to_trash = true,
+      constrain_cursor = "name",
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        padding = 5,
+        border = "rounded",
+      },
+    })
+    vim.keymap.set("n", "-", vim.cmd.Oil, { desc = "Open file explorer" })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,9 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"RRethy/nvim-treesitter-endwise",
-		"windwp/nvim-ts-autotag",
-	},
-	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"typescript",
-				"tsx",
-				"html",
-				"css",
-				"elixir",
-			},
-			endwise = {
-				enable = true,
-			},
-			sync_install = false,
-			auto_install = true,
-
-			build = ":TSUpdate",
-			event = { "BufRead", "BufNewFile" },
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = { enabled = true },
-		})
-
-		require("nvim-ts-autotag").setup()
-	end,
+  {
+    "RRethy/nvim-treesitter-endwise",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+  }
 }
